37500468ec9fe2c937843da21760a72e5a39500a
Feike Visser <fvisser@adobe.com>
2016-10-05 14:27:57 +0200

    DateFormatter example

---
+++ core/src/main/java/com/adobe/examples/htl/core/formatter/DateFormatter.java
@@ -0,0 +1,38 @@
+package com.adobe.examples.htl.core.formatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+
+/**
+ * Example to show formatting
+ * HTL-code: 
+ * <sly data-sly-use.date="${'com.adobe.examples.htl.core.formatter.DateFormatter' @ date=currentPage.lastModified,
+     format='dd/MM/yyyy'}">${date.value}</sly>
+ * 
+ * 
+ */
+
+@Model(adaptables=SlingHttpServletRequest.class)
+public class DateFormatter {
+	
+    @Inject
+    private Calendar date;
+
+    @Inject
+    private String format;
+
+    public String value;
+
+    @PostConstruct
+    protected void init() {
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        value = formatter.format(date.getTime());
+    }
+
+}
