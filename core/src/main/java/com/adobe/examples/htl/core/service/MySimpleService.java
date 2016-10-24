47143873904a23833b43ec8f32e8a7f3cb899861
Feike Visser <fvisser@adobe.com>
2016-10-24 11:26:18 +0200

    Example expanded with using other OSGi-service reference.

--- core/src/main/java/com/adobe/examples/htl/core/service/MySimpleService.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/MySimpleService.java
@@ -1,7 +1,9 @@
 package com.adobe.examples.htl.core.service;
 
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 
 @Service(value=MySimpleService.class)
 @Component
@@ -16,8 +18,11 @@ public class MySimpleService {
 	//
 	// https://issues.apache.org/jira/browse/SLING-4554
 	
+	@Reference
+	private ResourceResolverFactory resourceResolverFactory;
+	
 	public String getSimpleValue() {
-		return "hello";
+		return "hello " + resourceResolverFactory.getThreadResourceResolver().getUserID();
 	}
 
 }
