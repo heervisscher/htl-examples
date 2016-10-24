3fcefb6a31cc011b9ead7ea82d4da99b73990250
Feike Visser <fvisser@adobe.com>
2016-10-24 09:55:45 +0200

    new package

--- core/src/main/java/com/adobe/examples/htl/core/service/MySimpleService.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/MySimpleService.java
@@ -7,6 +7,15 @@ import org.apache.felix.scr.annotations.Service;
 @Component
 public class MySimpleService {
 	
+	
+	// you can use this service directly with data-sly-use
+	// like this example
+	// <sly data-sly-use.service="com.adobe.examples.htl.core.service.MySimpleService"/>
+	//
+	// ${service.simpleValue}
+	//
+	// https://issues.apache.org/jira/browse/SLING-4554?
+	
 	public String getSimpleValue() {
 		return "hello";
 	}
