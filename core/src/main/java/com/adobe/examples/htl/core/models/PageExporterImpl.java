202144038fe8666073a7366357cf62fb107459f3
Feike Visser <fvisser@adobe.com>
2017-03-17 11:24:25 +0100

    Using @Default

--- core/src/main/java/com/adobe/examples/htl/core/models/PageExporterImpl.java
+++ core/src/main/java/com/adobe/examples/htl/core/models/PageExporterImpl.java
@@ -4,8 +4,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 
 @Model(adaptables=Resource.class,resourceType="weretail/components/structure/page",adapters=PageExporter.class)
 @Exporter(name = "jackson", extensions = "json", selector="pageinfo")
@@ -14,7 +16,7 @@ public class PageExporterImpl implements PageExporter{
 	@Inject @Named("jcr:title")
 	private String title;
 	
-	@Inject
+	@Inject @Optional @Default(values="default")
 	private String name;
 
 	public String getTitle() {
