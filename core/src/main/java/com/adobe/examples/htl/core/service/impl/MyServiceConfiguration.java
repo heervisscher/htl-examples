e98f7260060ce7a4d5d03bed72f31563b29ffbfb
Feike Visser <fvisser@adobe.com>
2017-03-31 16:06:41 +0200

    Add extra type in the config.

--- core/src/main/java/com/adobe/examples/htl/core/service/impl/MyServiceConfiguration.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/impl/MyServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.adobe.examples.htl.core.service.impl;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @ObjectClassDefinition(name = "My Service Configuration", description = "Service Configuration")
@@ -8,5 +9,11 @@ public @interface MyServiceConfiguration {
 	
 	@AttributeDefinition(name = "Config Value", description = "Configuration value")
 	String configValue();
-
+	
+	@AttributeDefinition(name = "MultipleValues", description = "Multi Configuration values")
+	String[] getStringValues();
+	
+	@AttributeDefinition(name = "NumberValue", description = "Number values", type=AttributeType.INTEGER)
+	int getNumberValue();
+	
 }
