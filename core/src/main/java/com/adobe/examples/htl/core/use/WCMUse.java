a3d18b8c0a2c7878c02d5e3b88ddce55e9405288
Feike Visser <fvisser@adobe.com>
2017-04-01 15:35:59 +0200

    Example of new methods in AEM6.3

--- core/src/main/java/com/adobe/examples/htl/core/use/WCMUse.java
+++ core/src/main/java/com/adobe/examples/htl/core/use/WCMUse.java
@@ -1,5 +1,7 @@
 package com.adobe.examples.htl.core.use;
 
+import java.util.Locale;
+
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.day.cq.commons.jcr.JcrConstants;
 
@@ -7,16 +9,25 @@ public class WCMUse extends WCMUsePojo {
 
 	private String title;
 	
+	private Locale locale;
+	
 	@Override
 	public void activate() throws Exception {
+		// new in AEM6.3 : getInheritedPageProperties
 		// replaces getInheritedProperties()-method
 		title = getInheritedPageProperties().get(JcrConstants.JCR_TITLE, String.class);
+		
+		// also new in AEM6.3
+		// shortcut method getLanguage() without parameters
+		locale = getCurrentPage().getLanguage();
 	}
 	
 	public String getTitle() {
 		return title;
 	}
 	
+	public String getLanguage() {
+		return locale.getLanguage();
+	}
 	
-
 }
