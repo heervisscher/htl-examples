7b1268c157d5cf7cf627d5461ebd9b6e445cd300
Feike Visser <fvisser@adobe.com>
2017-04-01 14:16:27 +0200

    Adding logging

--- core/src/main/java/com/adobe/examples/htl/core/bindings/CustomBindingProvider.java
+++ core/src/main/java/com/adobe/examples/htl/core/bindings/CustomBindingProvider.java
@@ -5,6 +5,8 @@ import javax.script.Bindings;
 import org.apache.sling.scripting.api.BindingsValuesProvider;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMBindings;
 import com.adobe.examples.htl.core.models.MyCustomPage;
@@ -17,9 +19,13 @@ import com.day.cq.wcm.api.Page;
 	// make sure
 public class CustomBindingProvider implements BindingsValuesProvider {
 	
+	private static Logger log = LoggerFactory.getLogger(CustomBindingProvider.class);
+	
 	@Override
 	public void addBindings(Bindings bindings) {
+		log.info("In CustomBindingProvider..");
 		if ( bindings.containsKey(WCMBindings.CURRENT_PAGE)) {
+			log.info("Currentpage does exist..");
 			// there is the currentPage bindings from Sightly
 			Page current = (Page) bindings.get(WCMBindings.CURRENT_PAGE);
 			// adapt this to MyCustomPage, and add this to the bindings
@@ -27,6 +33,8 @@ public class CustomBindingProvider implements BindingsValuesProvider {
 			bindings.put("myPage", current.adaptTo(MyCustomPage.class));
 			// you can now refer to pageName and myPage in every HTML file
 			// example ${pageName} ${myPage.title}
+		} else {
+			log.info("Currentpage does not exist");
 		}
 	}
 }
