8baa9395e852c6002f6dd5db9f6fe645bc1d26fc
feike visser <heervisscher@gmail.com>
2017-06-11 09:55:22 +0200

    Create json.js

--- ui.apps/src/main/content/jcr_root/apps/aemhtlexamples/components/content/jsonwithjavascript/json.js
+++ ui.apps/src/main/content/jcr_root/apps/aemhtlexamples/components/content/jsonwithjavascript/json.js
@@ -1,4 +1,8 @@
  use(function () {
+  
+     // example on parsing handling JSON in Javascript, that is stored in JCR
+     // following JSON is stored in a String[] in the JCR
+     // {"key1":"value1","key2":"value2"},{"key1":"value3","key2":"value4"}
 
     var jsondata = currentPage.properties.get("jsondata");
     var jsonlength = currentPage.properties.get("jsondata").length;
@@ -6,6 +10,7 @@
     var keys = [];
 
     for (var i=0; i < jsonlength; i++) {
+        // parse the JSON, and get value for key1
         keys.push(JSON.parse(jsondata[i]).key1);
     }
 
