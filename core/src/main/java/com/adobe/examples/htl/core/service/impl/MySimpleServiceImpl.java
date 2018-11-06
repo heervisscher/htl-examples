e0c7e1e0e6c093492071af108d109a01dee02f11
Feike Visser <fvisser@adobe.com>
2018-11-06 09:17:03 +0100

    Using OSGi R7

--- core/src/main/java/com/adobe/examples/htl/core/service/impl/MySimpleServiceImpl.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/impl/MySimpleServiceImpl.java
@@ -5,12 +5,14 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.propertytypes.ServiceDescription;
 import org.osgi.service.metatype.annotations.Designate;
 
 import com.adobe.examples.htl.core.service.MySimpleService;
 
 @Component(service = MySimpleService.class,configurationPolicy=ConfigurationPolicy.REQUIRE)
 @Designate(ocd = MyServiceConfiguration.class)
+@ServiceDescription("My simple service")
 public class MySimpleServiceImpl implements MySimpleService {
 	
 	// to use the r7 OSGi annotations
