5502d06333f285c0b7ec12decf69ee595428bc03
Feike Visser <fvisser@adobe.com>
2018-11-06 11:53:43 +0100

    Using more r7 annotations

--- core/src/main/java/com/adobe/examples/htl/core/service/impl/MySimpleServiceImpl.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/impl/MySimpleServiceImpl.java
@@ -6,6 +6,8 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.propertytypes.ServiceDescription;
+import org.osgi.service.component.propertytypes.ServiceRanking;
+import org.osgi.service.component.propertytypes.ServiceVendor;
 import org.osgi.service.metatype.annotations.Designate;
 
 import com.adobe.examples.htl.core.service.MySimpleService;
@@ -13,6 +15,8 @@ import com.adobe.examples.htl.core.service.MySimpleService;
 @Component(service = MySimpleService.class,configurationPolicy=ConfigurationPolicy.REQUIRE)
 @Designate(ocd = MyServiceConfiguration.class)
 @ServiceDescription("My simple service")
+@ServiceRanking(1001)
+@ServiceVendor("Feike")
 public class MySimpleServiceImpl implements MySimpleService {
 	
 	// to use the r7 OSGi annotations
