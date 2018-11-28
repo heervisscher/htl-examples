48be8e1a34518e43a46d65302b0c4819d8f8d931
Feike Visser <fvisser@adobe.com>
2018-11-28 13:16:02 +0100

    more osgi r6/r7 examples

--- core/src/main/java/com/adobe/examples/htl/core/service/impl/MyServiceConfiguration.java
+++ core/src/main/java/com/adobe/examples/htl/core/service/impl/MyServiceConfiguration.java
@@ -3,6 +3,7 @@ package com.adobe.examples.htl.core.service.impl;
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.Option;
 
 @ObjectClassDefinition(name = "My Service Configuration", description = "Service Configuration")
 public @interface MyServiceConfiguration {
@@ -10,7 +11,9 @@ public @interface MyServiceConfiguration {
 	@AttributeDefinition(name = "Config Value", description = "Configuration value")
 	String configValue();
 	
-	@AttributeDefinition(name = "Multiple-Values", description = "Multi Configuration values")
+	@AttributeDefinition(name = "Multiple-Values", 
+			description = "Multi Configuration values, max 4",
+			cardinality = 4)
 	String[] getStringValues();
 	
 	@AttributeDefinition(name = "NumberValue", description = "Number values", type=AttributeType.INTEGER)
@@ -18,5 +21,15 @@ public @interface MyServiceConfiguration {
 	
 	@AttributeDefinition(description = "Single Configuration value")
 	String my_property_name(); // gets to my.property.name
+	
+	@AttributeDefinition(description = "Used method",
+    options = {
+             @Option(label = "GET", value = "GET"),
+             @Option(label = "POST", value = "POST"),
+             @Option(label = "PUT", value = "PUT"),
+             @Option(label = "PATCH", value = "PATCH"),
+             @Option(label = "DELETE", value = "DELETE")
+     })
+     String method();
 
 }
